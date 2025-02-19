83c5c37aeceb1cefce516ae670a38d8e1823c32c
Dominik Ledzianowski <dominik.ledzianowski@gmail.com>
2025-02-19 20:48:34 +0100

    V1.0

--- esp32_FirebasebyWiFi/src/main.cpp
+++ esp32_FirebasebyWiFi/src/main.cpp
@@ -3,60 +3,141 @@
 #include <Firebase_ESP_Client.h>
 #include <addons/TokenHelper.h>
 #include <addons/RTDBHelper.h>
-
 #include "PASSWORD.h"
 
+// Sensors lib
+#include <OneWire.h>
+#include <DallasTemperature.h>
+//#include <Wire.h>
+#include <Adafruit_INA219.h>
+
+
+// wysylanie -> dodac do FirebaseJson json
+// odbieranie ->dodac w reciveByWiFi if else i zmienna do struct RreadData
+
+// HA
+// sensor - dodac w configuration.yaml -> sensor: json_attributes
+// relay -  dodac configuration.yaml -> rest_command --> set_relay_01
+  
+
+// z 05 trzeba to co jest na 30 dni - acces
+// moze czytanie zminnych bez opozninia
+
+// Firebase
 FirebaseData fbdo;
 FirebaseAuth auth;
 FirebaseConfig config;
-
-unsigned long sendDataPrevMillis = 0;
 bool signupOK = false;
 
-// Zmienne do wysyłania danych
-int sent_nr_1 = 0;
-String URL_withPacket = "";
+// DS18B20
+#define DS18B20PIN 15
+OneWire oneWire(DS18B20PIN);
+DallasTemperature DS18B20sensors(&oneWire);
 
-// Zmienne do odbierania danych
-int received_nr_1 = 0;
-String received_text = "";
+// INA219
+Adafruit_INA219 ina219;
 
 
+struct SentData {
+    float DS18B20temp = 0.0;
+    float voltage = 0.0;
+    float current = 0.0;
+    float power = 0.0;
+}SentD;
+struct RreadData {
+    bool relay01 = false;
+}ReadD;
+
+
+void updateSensors();
+void reciveByWiFi();
+void sendByWiFi(FirebaseJson &json);
 void connectWiFi();
 void configFirebase();
 
-void setup(){
-    pinMode(LED_BUILTIN, OUTPUT);
 
-    Serial.begin(9600);//default baudrate for ESP
-    delay(4000);
+void setup(){
+    pinMode(LED_BUILTIN, OUTPUT);  // LED_BUILTIN
+    DS18B20sensors.begin();
+    if (!ina219.begin()) {
+        Serial.println("Failed to find INA219 sensor.");
+    }
+
+    Serial.begin(115200);
     connectWiFi();
     configFirebase();
 }
 
+
 void loop(){
-    if (WiFi.status() != WL_CONNECTED) {  // Jeśli straciliśmy połączenie WiFi, ponów próbę
+    if (WiFi.status() != WL_CONNECTED) {
         Serial.println("WiFi off! restart...");
         ESP.restart();
     }
-    
-    if (Firebase.ready() && signupOK){
-        if (Firebase.RTDB.setInt(&fbdo, "Sensor/PV", sent_nr_1)){
-            digitalWrite(LED_BUILTIN, HIGH);
-            Serial.printf("\n%d - successfully saved to: %s\n", sent_nr_1, fbdo.dataPath());
+
+    updateSensors();
+
+    // sending data
+    FirebaseJson json;
+    json.set("voltage_V", SentD.voltage);
+    json.set("current_mA", SentD.current);
+    json.set("power_mW", SentD.power);
+    json.set("DS18B20temp", SentD.DS18B20temp);
+    sendByWiFi(json);
+
+    // setting relays
+    reciveByWiFi();
+    analogWrite(BUILTIN_LED, ReadD.relay01);
+
+    delay(10000);
+}
+
+void updateSensors(){
+    // DS18B20
+    DS18B20sensors.requestTemperatures();
+    SentD.DS18B20temp = DS18B20sensors.getTempCByIndex(0);
+    SentD.DS18B20temp = round(SentD.DS18B20temp * 100) / 100.0;
+
+    // ina219
+    SentD.voltage = ina219.getBusVoltage_V();
+    SentD.current = ina219.getCurrent_mA();
+    SentD.power = ina219.getPower_mW();
+}
+
+void reciveByWiFi() {
+    if (Firebase.ready() && signupOK) {
+        if (Firebase.RTDB.getJSON(&fbdo, "HAtoESP/")) {
+            FirebaseJson &json = fbdo.jsonObject();
+            FirebaseJsonData jsonData;
+            
+            // saving data from json
+            if (json.get(jsonData, "relay01") && jsonData.type == "boolean") {
+                ReadD.relay01 = jsonData.boolValue; // Zapis do struktury
+            } else {
+                Serial.println("FAILED: 'relay01' not found or wrong type.");
+            }
+
+            //
+            // DODAC IF ELSE z "nowa zmienna" z bazy danych
+            //
+        } else {
+            Serial.printf("FAILED READ: %s\n", fbdo.errorReason().c_str());
         }
-        else{
-            Serial.printf("FALIED: %s\n", fbdo.errorReason());
+    } else {
+        Serial.println("Firebase not ready or sign up failed");
+    }
+}
+
+void sendByWiFi(FirebaseJson &json){
+    if (Firebase.ready() && signupOK){
+        //setJSONAsync() -> dla wyslania w tle
+        if (!Firebase.RTDB.setJSON(&fbdo, "ESPtoHA", &json)) {
+            Serial.printf("FAILED send: %s\n", fbdo.errorReason().c_str());
         }
     }
     else{
-    Serial.println("Firebase not ready or sign up failed");
+        Serial.println("Firebase not ready or sign up failed");
     }
-    delay(100);
-    digitalWrite(LED_BUILTIN, LOW);
-    delay(4900);
-
-    sent_nr_1 += 1;
 }
 
 void connectWiFi(){
@@ -71,13 +152,13 @@ void connectWiFi(){
         retry++;
     }
 
-    if (WiFi.status() == WL_CONNECTED){
-        Serial.print("Polalaczono z WiFi! --- IP: ");
-        Serial.println(WiFi.localIP());
-    }
-    else{
+    if (WiFi.status() != WL_CONNECTED){
         Serial.print("\nNie udalo sie polaczyc z WiFi.");
         ESP.restart();
+    }
+    else{
+        Serial.print("Polalaczono z WiFi! --- IP: ");
+        Serial.println(WiFi.localIP());
     }
 }
 
